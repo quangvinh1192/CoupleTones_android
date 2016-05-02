2b7478faf0c63d5b1e8fde8b9042d8b05d0f9b3d
Trung <dotrantrung@gmail.com>
2016-05-01 21:35:28 -0700

    50% add a location

--- app/src/main/java/com/example/group26/coupletones/favMap.java
+++ app/src/main/java/com/example/group26/coupletones/favMap.java
@@ -1,18 +1,25 @@
 package com.example.group26.coupletones;
 
+import android.gesture.GestureOverlayView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class favMap extends FragmentActivity implements OnMapReadyCallback {
+public class favMap extends FragmentActivity implements OnMapReadyCallback    {
 
     private GoogleMap mMap;
+    private Button addPlaceBtn;
+    private boolean addingMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +29,19 @@ public class favMap extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+        addPlaceBtn = (Button)findViewById(R.id.addPlaceBtnID);
+
+        addPlaceBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d("MyApp", "I am here");
+                addingMode = true;
+                addPlaceBtn.setEnabled(false);
+            }
+        });
+
+
+    };
 
 
     /**
@@ -42,5 +61,13 @@ public class favMap extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng point) {
+                mMap.addMarker(new MarkerOptions().position(point).title("Marker in Sydney"));
+            }
+
+        });
+
     }
 }
