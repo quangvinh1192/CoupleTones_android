4ce61dd4d312ead3e8ea8b145ee2d31b2c4fd746
jkapich <jkapich@gmail.com>
2016-05-06 16:39:44 -0700

    added add a spouse page and added textviews and buttons to the page

---
+++ app/src/main/java/com/example/group26/coupletones/AddSpousePage.java
@@ -0,0 +1,13 @@
+package com.example.group26.coupletones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSpousePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_spouse_page);
+    }
+}
