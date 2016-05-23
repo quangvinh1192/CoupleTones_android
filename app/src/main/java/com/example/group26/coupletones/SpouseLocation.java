c2be0f87202ad4dfde7c0a0623cbd901ddc8f9fe
Gagandeep Singh <g9singh@ucsd.edu>
2016-05-22 21:49:03 -0700

    edited SpouseLocation a little bit

--- app/src/main/java/com/example/group26/coupletones/SpouseLocation.java
+++ app/src/main/java/com/example/group26/coupletones/SpouseLocation.java
@@ -4,6 +4,8 @@ import android.location.Location;
 import android.os.Vibrator;
 import android.support.v7.app.NotificationCompat;
 
+import java.sql.Date;
+
 /**
  * Created by gagan on 5/22/16.
  */
@@ -12,6 +14,8 @@ public class SpouseLocation {
     public String name;
     public String vibrationLevel;
     public String soundType;
+    public boolean visited;
+    public Date dateVisited;
 
     /* Constructor for a SpouseLocation
      * Parameters: Location and a String which holds the vibration type
@@ -47,5 +51,22 @@ public class SpouseLocation {
     //gets the vibrationLevel "either low medium or high"
     public String getVibrationLevel(){
         return vibrationLevel;
+    }
+
+    //checks if this location was visited or not
+    public boolean isVisited() {
+        return visited;
+    }
+
+    //checks what time the location was visited
+    public Date getTimeVisited() {
+        return dateVisited;
+    }
+
+    //when you visit a place make sure to call this method and pass in true, Date()
+    //Date() will create an object with the current date and time
+    public void setVisited(boolean visited, Date dateAndTime){
+        this.visited = visited;
+        dateVisited = dateAndTime;
     }
 }
