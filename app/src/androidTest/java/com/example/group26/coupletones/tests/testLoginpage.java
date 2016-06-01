0fd872cd433a0c52a967252dd3db2373df1bf4ed
Vinh Tran <quangvinh1192@gmail.com>
2016-06-01 11:18:30 -0700

    first test on email and password is missing

--- app/src/androidTest/java/com/example/group26/coupletones/tests/testLoginpage.java
+++ app/src/androidTest/java/com/example/group26/coupletones/tests/testLoginpage.java
@@ -12,11 +12,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * Created by admin_me on 5/31/16.
@@ -28,11 +33,10 @@ public class testLoginpage {
     public ActivityTestRule<loginPage> testloginPage= new ActivityTestRule<>(loginPage.class);
 
     @Test
-    public void sayHello(){
-        //onView(withId(R.id.editText)).perform(typeText("vinh"), ViewActions.closeSoftKeyboard());
-        //onView(withId(R.id.button)).perform(click());
-        //String expectedText= "vinh";
-        //onView(withId(R.id.textView)).check(matches(withText(expectedText)));
-        //onView(withId(R.id.editText)).check(matches(withText(expectedText)));
+    public void testLogin(){
+        onView(withId(R.id.emailTV)).perform(typeText("quangvinh1192@gmail.com"), closeSoftKeyboard());
+        onView(withId(R.id.passwordTV)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.submitButton)).perform(click());
+        onView(withText("Email or password is missing")).check(matches(isDisplayed()));
     }
 }
